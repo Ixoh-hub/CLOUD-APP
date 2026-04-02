a4fd04af4929782dd4243bd05f37129c3615d747
Azibo Isaac Alinda <kingixoh@gmail.com>
2026-04-03 00:22:22 +0300

    db issue

--- server.js
+++ server.js
@@ -82,6 +82,6 @@ app.delete("/posts/:id", async (req, res) => {
 
 // PORT
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
